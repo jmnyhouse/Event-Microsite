5e00cb1efda946e7950164cc7cfddac4afd2d2e0
jmnyhouse <159553624+jmnyhouse@users.noreply.github.com>
2024-02-29 10:37:41 -0600

    Add files via upload

---
+++ js/custom_behaviors.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+					
+	// VARIABLES
+	
+	
+	// EVENT HANDLERS
+	
+	
+	// FUNCTIONS
+	import * as addToCalendarButton from "https://cdn.skypack.dev/add-to-calendar-button@1.4.0";
+
+addToCalendarButton.atcb_init();
+	
+
+
+
+	// INITIALIZATION
+	
+	
+});
